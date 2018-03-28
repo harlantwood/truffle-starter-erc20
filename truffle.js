6ad29c8516edeffb752298d11a344a6f8f5aad3c
Harlan T Wood <code+harlan@harlantwood.net>
2018-03-27 15:40:50 -1000

    dev only, on 8545

--- truffle.js
+++ truffle.js
@@ -1,25 +1,9 @@
-const LightWalletProvider = require('@digix/truffle-lightwallet-provider');
-
-const { KEYSTORE, PASSWORD } = process.env;
-
-if (!KEYSTORE || !PASSWORD) { throw new Error('You must export KEYSTORE and PASSWORD (see truffle.js)'); }
-
 module.exports = {
   networks: {
     development: {
       host: 'localhost',
-      port: 6545,
+      port: 8545,
       network_id: '*',
-    },
-    kovan: {
-      network_id: '42',
-      provider: new LightWalletProvider({
-        keystore: KEYSTORE,
-        password: PASSWORD,
-        rpcUrl: 'https://kovan.infura.io/',
-        // rpcUrl: 'http://localhost:8545/',
-        pollingInterval: 2000,
-      }),
     },
   },
 };
